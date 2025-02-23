47c596a71bb916810066543861b750fd4260dde2
Fernanda Baeza <mafer370@gmail.com>
2025-02-23 09:55:48 -0600

    Correciones

--- scripts/index.js
+++ scripts/index.js
@@ -1,72 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let buttonEdit = document.querySelector(".profile__edit");
-  let openPopup = document.querySelector(".popup");
-  let buttonClose = document.querySelector(".popup__close");
-  let closePopup = document.querySelector(".popup");
-  let buttonsLike = document.querySelectorAll(".element__button");
-  let inputName = document.querySelector(".form__input_name");
-  let inputAbout = document.querySelector(".form__input_about");
-  let textTitle = document.querySelector(".profile__title");
-  let textText = document.querySelector(".profile__text");
-  let textName = textTitle.textContent;
-  let textAbout = textText.textContent;
-  let buttonSubmit = document.querySelector(".form__submit");
-  const loaderTimeout = 5000;
+const openPopup = document.querySelector(".popup");
+const buttonPopup = document.querySelector(".profile__info-button");
+const buttonClosePopup = document.querySelector(".popup__button-close");
+const formElement = document.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__form-name");
+const jobInput = document.querySelector(".popup__form-occupation");
+const displayName = document.querySelector(".profile__details-name");
+const displayDescription = document.querySelector(
+  ".profile__details-description"
+);
+const buttonSubmit = document.querySelector(".popup__form-button");
 
-  function open() {
-    if (openPopup.style.display === "none" || openPopup.style.display === "") {
-      openPopup.style.display = "flex";
-      document.getElementById("loader").style.display = "block";
-      document.getElementById("loader__circle").style.display = "none";
-      setTimeout(function () {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("loader__circle").style.display = "block";
-        inputName.value = textName;
-        inputAbout.value = textAbout;
-        buttonSubmit.style.backgroundColor = "#000000";
-      }, loaderTimeout);
-    } else {
-      openPopup.style.display = "none";
-    }
-  }
+function editProfile() {
+  handlePopupOpen();
+  nameInput.value = displayName.textContent;
+  jobInput.value = displayDescription.textContent;
+}
 
-  function close() {
-    if (closePopup.style.display === "flex") {
-      closePopup.style.display = "none";
-      inputName.value = "";
-      inputAbout.value = "";
-      buttonSubmit.style.backgroundColor = "#FFFFFF";
-    } else {
-      closePopup.style.display = "flex";
-    }
-  }
+function handlePopupOpen() {
+  openPopup.classList.add("popup_opened");
+}
 
-  function like() {
-    let button = event.target;
-    let currentBackground = window.getComputedStyle(button).backgroundImage;
-    if (currentBackground.includes("btn-like-nr.png")) {
-      button.style.backgroundImage = 'url("./images/btn-like-ac.png")';
-    } else {
-      button.style.backgroundImage = 'url("./images/btn-like-nr.png")';
-    }
-  }
+function handlePopupClose() {
+  openPopup.classList.remove("popup_opened");
+}
 
-  function handleProfileFormSubmit(evt) {
-    evt.preventDefault();
-    console.log(inputName.value);
-    console.log(inputAbout.value);
-    textTitle.textContent = inputName.value;
-    textText.textContent = inputAbout.value;
-    closePopup.style.display = "none";
-    inputName.value = "";
-    inputAbout.value = "";
-    buttonSubmit.style.backgroundColor = "#FFFFFF";
-  }
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  displayName.textContent = nameInput.value;
+  displayDescription.textContent = jobInput.value;
+  openPopup.classList.remove("popup_opened");
+}
 
-  buttonClose.addEventListener("click", close);
-  buttonsLike.forEach((button) => {
-    button.addEventListener("click", like);
-  });
-  buttonSubmit.addEventListener("click", handleProfileFormSubmit);
-  buttonEdit.addEventListener("click", open);
-});
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+buttonPopup.addEventListener("click", handlePopupOpen);
+buttonPopup.addEventListener("click", editProfile);
+buttonClosePopup.addEventListener("click", handlePopupClose);
